1634622a703c6b93f8bb14653db2b8d95fd8906e
Ombo16 <ombo.mcboaz@gmail.com>
2023-10-17 16:10:09 -0700

    printf collabo

--- main.h
+++ main.h
@@ -1,7 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
+#define UNUSED(x) (void)(x)
+#define BUF_SIZE 1024
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+void scan_format(const char *format, va_list args, int *count, char buffer[]);
+void conv_specifier(char specifier, va_list args, int *count, char buffer[]);
 
 #endif
