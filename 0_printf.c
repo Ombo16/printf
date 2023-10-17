4ec3e654422b3ed180eaa576347ba5827ccf3e0c
Ombo16 <ombo.mcboaz@gmail.com>
2023-10-17 10:47:48 -0700

    Printf collabo

--- 0_printf.c
+++ 0_printf.c
@@ -3,27 +3,70 @@
 #include "main.h"
 
 /**
- * _printf - A simplified version of printf that handles 'c', 's', 'd', 'i', and '%' specifiers.
+ * _printf - A simplified version of printf that
+ * handles 'c', 's', 'd', 'i', and '%' specifiers.
  *
  * @format: The format string.
  * @...: Variable number of additional arguments.
  *
- * Return: The number of characters printed (excluding the null byte used to end output to strings).
+ * Return: The number of characters printed
+ * (excluding the null byte used to end output to strings).
  */
-int _printf(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
-    int count;
+int _printf(const char *format, ...)
+{
+	va_list args;
+	va_start(args, format);
 
-    count = vprintf(format, args);
+	int count = 0;
 
-    va_end(args);
-    return count;
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '\0')
+				break;
+			if (*format == 'c')
+			{
+				char c = va_arg(args, int);
+				_putchar(c);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				const char *str = va_arg(args, const char *);
+				while (*str != '\0')
+				{
+					_putchar(*str);
+					str++;
+					count++;
+				}
+			}
+			else if (*format == '%')
+				{
+				_putchar('%');
+				count++;
+				}
+				else
+				{
+				}
+		}
+		else
+		{
+			_putchar(*format);
+			count++;
+		}
+
+		format++;
+	}
+
+	va_end(args);
+	return count;
 }
 
-int main() {
-    int printed = _printf("This is a test: %c, %s, %d, %i, %%\n", 'A', "Hello, World!", 42, -123);
-    printf("Number of characters printed: %d\n", printed);
-    return 0;
+int main()
+{
+	int printed = _printf("This is a test: %c, %s, %%\n", 'A', "Let's try to printf a simple sentence.\n");
+	printf("Number of characters printed: %d\n", printed);
+	return 0;
 }
-
