361a92afb0633893a779b1d76ef568e06e2ee3be
Ombo16 <ombo.mcboaz@gmail.com>
2023-10-17 10:16:44 -0700

    Printf collabo

--- 0_printf.c
+++ 0_printf.c
@@ -10,62 +10,20 @@
  *
  * Return: The number of characters printed (excluding the null byte used to end output to strings).
  */
-int _printf(const char *format, ...)
-{
-	va_list args;
-	va_start(args, format);
+int _printf(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    int count;
 
-	int count = 0;
+    count = vprintf(format, args);
 
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == '\0')
-				break;
-			if (*format == 'c')
-			{
-				char c = va_arg(args, int);
-				_putchar(c);
-				count++;
-			}
-			else if (*format == 's')
-			{
-				const char *str = va_arg(args, const char *);
-				while (*str != '\0')
-				{
-					_putchar(*str);
-					str++;
-					count++;
-				}
-			}
-			else if (*format == '%')
-				{
-				_putchar('%');
-				count++;
-				}
-				else
-				{
-				}
-		}
-		else
-		{
-			_putchar(*format);
-			count++;
-		}
-
-		format++;
-	}
-
-	va_end(args);
-	return count;
+    va_end(args);
+    return count;
 }
 
-int main()
-{
-	int printed = _printf("This is a test: %c, %s, %%\n", 'A', "Hello, World!");
-	printf("Number of characters printed: %d\n", printed);
-	return 0;
+int main() {
+    int printed = _printf("This is a test: %c, %s, %d, %i, %%\n", 'A', "Hello, World!", 42, -123);
+    printf("Number of characters printed: %d\n", printed);
+    return 0;
 }
 
